6eb720f76267d7c4e92bf1a8410ee9cb57939e40
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-07-24 23:09:04 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cout << i << "\n";
+    }
+}
