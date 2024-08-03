4c39afcec45cd01d1b6882c8e0026e6a82dad160
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-08-03 10:04:09 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<int> path;
+int visited[9] = { 0 };
+
+void dfs(int level) {
+    if (level == m) {
+        for (auto s : path) {
+            cout << v[s] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!path.empty() && path.back() >= i) {
+            continue;
+        }
+        path.push_back(i);
+
+        dfs(level + 1);
+
+        path.pop_back();
+    }
+}
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    dfs(0);
+}
