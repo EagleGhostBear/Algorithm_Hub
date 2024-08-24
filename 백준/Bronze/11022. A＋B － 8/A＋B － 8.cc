f7af03c2ff48f55f647ca47ee184b4b8b8a98d7c
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-08-24 23:45:10 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int t;
+
+int main(int argc, char** argv) {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+    }
+}
