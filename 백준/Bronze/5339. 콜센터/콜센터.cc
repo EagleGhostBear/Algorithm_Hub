aae2e2ed3967212c116d7a49dcf78faacfc9adf5
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-08-27 19:57:58 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout << "     /~\\" << endl;
+    cout << "    ( oo|" << endl;
+    cout << "    _\\=/_" << endl;
+    cout << "   /  _  \\" << endl;
+    cout << "  //|/.\\|\\\\" << endl;
+    cout << " ||  \\ /  ||" << endl;
+    cout << "============" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+}
