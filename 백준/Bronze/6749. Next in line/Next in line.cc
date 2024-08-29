33e7da3b2255fe6b279e55affaa08b76d91938bd
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-08-29 20:42:58 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int t;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int a, b;
+    cin >> a >> b;
+    cout << 2*b-a;
+}
