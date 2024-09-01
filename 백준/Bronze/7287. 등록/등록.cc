964d90b0e4f8298042c61be372efacb47b48550f
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-09-01 16:56:14 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    cout << "99\n";
+    cout << "ghost11062";
+}
