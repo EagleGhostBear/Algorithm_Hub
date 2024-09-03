d7cb7b540cebdcee2cce4727b2e6bb1e37e4b8cf
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-09-03 17:21:03 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << a * b + c * d;
+}
