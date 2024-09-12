d9c55bd8420fa8ee58728da826d3f06299a75be6
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-09-12 14:51:53 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout<<"|\\_/|"<<endl;
+    cout<<"|q p|   /}"<<endl;
+    cout<<"( 0 )\"\"\"\\"<<endl;
+    cout<<"|\"^\"`    |"<<endl;
+    cout<<"||_/=\\\\__|"<<endl;
+}
