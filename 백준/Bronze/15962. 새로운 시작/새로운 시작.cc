7b144cfeba8c23f71275747103a647e213df59d1
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-10-10 19:42:46 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+long long a, b;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cout << "파이팅!!";
+}
