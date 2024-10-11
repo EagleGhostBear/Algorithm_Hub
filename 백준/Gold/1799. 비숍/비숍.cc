c94fcca5eea990b4a3af0e57fe6d925cd1e515bc
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-10-11 17:18:43 +0900

    [Gold I] Title: 비숍, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.cc
+++ 백준/Gold/1799. 비숍/비숍.cc
@@ -1,29 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <string>
-#include <deque>
 
 using namespace std;
 
 int n;
 vector<int> ans(2);
-vector<vector<int>> v;
+vector<vector<pair<int, int>>> v(2);
 vector<bool> slab1(19), slab2(19);
 
 void dfs(int idx, int cnt, int color) {
-    for (int i = idx + 1; i < n * n; i++) {
-        int row = i / n;
-        int col = i % n;
-        if ((row + col) % 2 != color) { continue; } // 못놓는 색상 생략
-        if (!v[row][col]) { continue; } // 비숍을 못 놓는 위치는 생략
+    for (int i = idx + 1; i < v[color].size(); i++) {
+        int row = v[color][i].first;
+        int col = v[color][i].second;
         if (slab1[row + col] || slab2[row - col + (n - 1)]) { continue; } // 대각선이 중복되는 위치 생략
 
-        v[row][col] = 0;
         slab1[row + col] = true;
         slab2[row - col + (n - 1)] = true;
         dfs(i, cnt + 1, color);
-        v[row][col] = 1;
         slab1[row + col] = false;
         slab2[row - col + (n - 1)] = false;
     }
@@ -37,10 +30,13 @@ int main() {
     ios_base::sync_with_stdio(0);
 
     cin >> n;
-    v.resize(n, vector<int>(n));
+    v.resize(2, vector<pair<int, int>>(2 * n - 1));
+
+    int tmp;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> v[i][j];
+            cin >> tmp;
+            if (tmp) { v[(i + j) % 2].push_back({ i, j }); } // 흑, 백 칸에 둘 수 있는만 따로 저장
         }
     }
 
