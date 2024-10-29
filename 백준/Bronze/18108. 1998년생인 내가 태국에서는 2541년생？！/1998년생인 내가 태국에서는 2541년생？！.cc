53f541a2231848c0fc860ba1a27a5e3c2f62a15a
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-10-29 17:46:05 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> n;
+    cout << n - 543;
+}
