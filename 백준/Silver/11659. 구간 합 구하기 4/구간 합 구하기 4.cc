fac619c93f922aff64a50adec4951b9218bb4359
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-11-05 11:57:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -6,8 +6,7 @@ using namespace std;
 typedef long long ll;
 
 int n, m;
-vector<int> v;
-vector<ll> dp;
+vector<ll> v;
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -15,24 +14,21 @@ int main() {
 
     cin >> n >> m;
     v.resize(n);
-    dp.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> v[i];
-    }
-
-    dp[0] = v[0];
-    for (int i = 1; i < n; i++) {
-        dp[i] = dp[i - 1] + v[i];
+        if (i) {
+            v[i] += v[i - 1];
+        }
     }
 
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         if (a == 1) {
-            cout << dp[b - 1] << "\n";
+            cout << v[b - 1] << "\n";
         }
         else {
-            cout << dp[b - 1] - dp[a - 2] << "\n";
+            cout << v[b - 1] - v[a - 2] << "\n";
         }
     }
 }
