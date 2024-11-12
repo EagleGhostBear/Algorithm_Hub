0f171390f42366bef75f9190db0541592a55c11c
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-11-12 19:27:01 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<int> v;
+vector<int> dp;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    v.resize(n);
+    dp.resize(n, 0);
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i-1; j > -1; j--) {
+            if (v[j] < v[i]) {
+                dp[i] = max(dp[i], dp[j]);
+            }
+        }
+        dp[i] += 1;
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+}
