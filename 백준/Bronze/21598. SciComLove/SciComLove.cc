7af2cb1227e3d0ff73a770dde01dc5f149a477dd
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-11-17 18:51:34 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<int> v;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cout << "SciComLove\n";
+    }
+}
