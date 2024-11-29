660f4f2e3a6a4867fb891ddb086ac01d640af42b
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-11-29 14:26:30 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n, m;
+vector<int> v;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+}
