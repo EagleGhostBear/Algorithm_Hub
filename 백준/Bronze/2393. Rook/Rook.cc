b18f517fa78f54f33a671d8f586a44a5417112e9
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-12-08 22:51:35 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<vector<int>> v;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\\n";
+}
