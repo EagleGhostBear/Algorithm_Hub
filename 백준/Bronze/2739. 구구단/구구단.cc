da099bbbcd1cfdc4fd97f4d7e72ec84dcd6bb99b
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-12-11 21:45:41 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<vector<int>> v;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    for(int i=1; i<10; i++){
+        cout << n << " * " << i << " = " << n*i << "\n";
+    }
+}
