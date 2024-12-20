453abd18c77faaab26786612aafd3bae4e4711d6
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-12-21 02:20:00 +0900

    [Bronze IV] Title: 특식 배부, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<int> v;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> n;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << min(n, a) + min(n, b) + min(n, c);
+}
