67d77aac3a5cce9161113ef6f8e2dde89f04f414
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2024-12-24 16:25:36 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -20,54 +20,33 @@ int main() {
 
     cin >> t;
     while(t--){
-        ll from, to, target, speed = 1, cnt = 0;
+        ll from, to, target, speed = 0, cnt = 0;
         cin >> from >> to;
         target = to - from;
 
-        if (target < 2) {
-            target -= 1;
-            cnt += 1;
-            //cout << "first 1 STEP: " << speed << endl;
-        }
-        else {
-            target -= 2;
-            cnt += 2;
-            //cout << "first 2 STEP: " << 2*speed << endl;
-        }
-
         while (target) {
-            if (target >= 2*(speed + 1)) {
+            if (target >= 2*(speed + 1)) { // 스피드가 증가 될때 내려오는 것까지 생각해서 *2
                 speed += 1;
                 target -= (2 * speed);
-                cnt += 2;
-                //cout << "2 STEP: " << 2*speed << endl;
+                cnt += 1;
             }
-            else if (target >= speed + 1) {
+            else if (target >= speed + 1) { // speed 증가가 1번만
                 speed += 1;
                 target -= speed;
-                cnt += 1;
-                //cout << "1 STEP: " << speed << endl;
             }
-            else if (target >= speed) {
-                target -= speed;
-                cnt += 1;
-                //cout << "1 STEP: " << speed << endl;
-            }
-            else {
+            else { // 현재 나왔던 속도들 중 1개의 속도를 그대로 유지하면 target 값에 도달한다는 의미
                 target = 0;
-                cnt += 1;
-                //cout << "last STEP: " << speed << endl;
             }
+            cnt += 1;
         }
 
         cout << cnt << "\n";
     }
 }
 /*
-x, y <= int 마지노선
+x, y <= int 마지노선, long long 사용
 다음 속도 => -1, 0, 1
 
-결국 올라간 속도는 다시 내려와야 한다
+결국 올라간 속도는 다시 내려와야 한다 -> 속도 증가를 *2 로 생각하여 내려오는것 까지 생각
 목표 거리 = (y - x)
-k(짝수) = 1(2) 
 */
