155dde04d5d39dd0425c5e1abff0447df504f215
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-02-11 17:16:57 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -12,25 +12,25 @@ typedef long long ll;
 
 
 int t, n;
+vector<int> dp(1000001, 0); // dp[총합] = 방법의 수
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> t;
+    dp[1] = 1; dp[2] = 1; dp[3] = 1;
+    for (int i = 1; i <= 1000000; i++) {
+        if (i - 1 > -1) dp[i] += dp[i - 1];
+        if (dp[i] > 1000000009) dp[i] %= 1000000009;
+        if (i - 2 > -1) dp[i] += dp[i - 2];
+        if (dp[i] > 1000000009) dp[i] %= 1000000009;
+        if (i - 3 > -1) dp[i] += dp[i - 3];
+        if (dp[i] > 1000000009) dp[i] %= 1000000009;
+    }
+
     while (t--) {
         cin >> n;
-        vector<int> dp(n+1, 0); // dp[총합] = 방법의 수
-        dp[1] = 1; dp[2] = 1; dp[3] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            if (i - 1 > -1) dp[i] += dp[i - 1];
-            if (dp[i] > 1000000009) dp[i] %= 1000000009;
-            if (i - 2 > -1) dp[i] += dp[i - 2];
-            if (dp[i] > 1000000009) dp[i] %= 1000000009;
-            if (i - 3 > -1) dp[i] += dp[i - 3];
-            if (dp[i] > 1000000009) dp[i] %= 1000000009;
-        }
         cout << dp[n] << "\n";
     }
 }
