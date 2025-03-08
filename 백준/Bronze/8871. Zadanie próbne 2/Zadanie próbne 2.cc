11b6bb6b8d14c4aae805e4255e4c37a21c432599
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-03-08 21:45:31 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    cout << (n+1)*2 << " " << (n+1)*3;
+}
