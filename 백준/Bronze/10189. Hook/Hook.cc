b09aa4c3bc1625a1b98a7e91025356b70b456851
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-03-11 20:22:30 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cout << "#  # #### #### #  #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#  # #### #### #  #" << '\n';
+}
