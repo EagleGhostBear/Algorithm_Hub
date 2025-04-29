f4cc6154d87984b422d6d687bb8bb2b30239f919
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-04-29 20:37:14 +0900

    [level 1] Title: 추억 점수, Time: 0.65 ms, Memory: 4.59 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<string, int> um;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    for(int i=0; i<name.size(); i++) um[name[i]] = yearning[i];
+    
+    for(int i=0; i<photo.size(); i++){
+        int score = 0;
+        for(int j=0; j<photo[i].size(); j++) score += um[photo[i][j]];
+        answer.push_back(score);
+    }
+    return answer;
+}
