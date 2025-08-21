95ce8a2d021a7b00e6df1eb9cc80809250380905
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-08-21 23:59:07 +0900

    [Bronze III] Title: 소음, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2935. 소음/소음.cc
+++ 백준/Bronze/2935. 소음/소음.cc
@@ -1,47 +1,46 @@
 #include <iostream>
- 
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <cmath>
+
 using namespace std;
- 
+
+typedef long long ll;
+
+string a, c;
+char b;
+
 int main() {
- 
-    string str, str1;
-    char ch;
- 
-    cin>>str>>ch>>str1;
- 
-    int strsize = str.length();
-    int str1size = str1.length();
- 
-    // * 일때 str, str1 수 0의개수 파악
-    if(ch=='*'){
-        int totalsize = strsize + str1size -2;
-        cout<<'1';
-        for(int i=0; i<totalsize; i++){
-            cout<<0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> a >> b >> c;
+    if (b == '+') {
+        if (a.size() == c.size()) {
+            a[0] = '2';
+            cout << a;
+        }
+        else if (a.size() > c.size()) {
+            a[a.size() - c.size()] = '1';
+            cout << a;
+        }
+        else if (a.size() < c.size()) {
+            c[c.size() - a.size()] = '1';
+            cout << c;
         }
     }
- 
-    else{
-        // + 일때 두수의 자릿수가 같을 때
-        if(strsize==str1size){
-            cout<<'2';
-            for(int i=0; i<strsize-1; i++){
-                cout<<'0';
-            }
-        }
-        // + 일때 두수의 자릿수가 다를 때
-        else if(strsize!=str1size){
-            int a=max(strsize, str1size);       // keypoint max(a,b) a,b 중 가장 큰값 출력
-            int b=min(strsize, str1size);       //          min(a,b) a.b 중 가장 작은값 출력
-                for(int i=a; i>0; i--){         // ex) a=4 -> 4321 
-                    if(i==a||i==b){             // ex) a=4, b=2 -> 1010
-                        cout<<'1';
-                    }
-                    else{
-                        cout<<'0';
-                    }
-                }
-            }
-        }
-        return 0;
+    else if (b == '*') {
+        cout << "1";
+        for (int i = 0; i < a.size() - 1; i++) cout << "0";
+        for (int i = 0; i < c.size() - 1; i++) cout << "0";
+    }
 }
+/*
+
+*/
