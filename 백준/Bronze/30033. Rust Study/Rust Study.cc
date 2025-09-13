823f74c32a18681a5f168ef07bdbd882befa7fa2
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-09-13 19:56:24 +0900

    [Bronze IV] Title: Rust Study, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30033. Rust Study/Rust Study.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<int> v;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    v.resize(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    int a, ans = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a >= v[i]) ans++;
+    }
+    cout << ans;
+}
+/*
+
+*/
