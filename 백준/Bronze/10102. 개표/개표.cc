37e8c3d72919c7b2e459e4e8baf9b99ca451987c
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-10-11 20:00:06 +0900

    [Bronze III] Title: 개표, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+vector<int> v(2);
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        v[s[i] - 'A']++;
+    }
+    if (v[0] > v[1]) cout << "A";
+    else if (v[0] < v[1]) cout << "B";
+    else cout << "Tie";
+}
