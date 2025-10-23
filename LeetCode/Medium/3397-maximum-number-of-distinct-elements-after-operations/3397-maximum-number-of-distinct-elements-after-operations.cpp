a8a908941bb6ffe6f04e942598c864176bf22002
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-10-23 16:15:51 +0900

    [Medium] Title: 3397-maximum-number-of-distinct-elements-after-operations, Time: 203 ms (11.83%), Memory: 124 MB (11.65%) -LeetHub-3.0

--- LeetCode/Medium/3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
+++ LeetCode/Medium/3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
         map<int, int> m;
-        queue<pair<int, int>> q;
         for(int i=0; i<nums.size(); i++) m[nums[i]]++;
-        for(auto n : m) q.push({n.first, n.second});
 
         pair<int, int> num = {-1e9, 0}; // val, cnt
-        while(q.size()){
-            if(num.first < q.front().first - k) num.first = q.front().first - k;
-            else if(num.first > q.front().first + k) q.pop();
-            else num.first++, num.second++, q.front().second--;
-
-            if(q.front().second == 0) q.pop(); 
+        for(auto it = m.begin(); it != m.end();){
+            if(num.first < it->first - k) num.first = it->first - k;
+            else if(num.first > it->first + k) it++;
+            else{
+                num.first++;
+                num.second++;
+                it->second--;
+                if(it->second == 0) it++;
+            }
         }
         return num.second;
     }
