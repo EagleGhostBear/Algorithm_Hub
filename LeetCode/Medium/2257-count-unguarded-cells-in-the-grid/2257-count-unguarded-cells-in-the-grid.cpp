d647e9107577a36b6108a0ed0dc2c4595bac4391
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-11-05 07:17:20 +0900

    [Medium] Title: 2257-count-unguarded-cells-in-the-grid, Time: 119 ms (33.19%), Memory: 193.8 MB (31.71%) -LeetHub-3.0

--- LeetCode/Medium/2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ LeetCode/Medium/2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -11,16 +11,15 @@ public:
             v[walls[i][0]][walls[i][1]] = 3;
             ans--;
         }
-        for(int i=0; i<guards.size(); i++){
-            v[guards[i][0]][guards[i][1]] = 2;
-            ans--;
-        }
+
+        queue<node> q;
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, 1, 0, -1};
         for(int i=0; i<guards.size(); i++){
-            int row = guards[i][0];
-            int col = guards[i][1];
-            queue<node> q;
+            if(v[guards[i][0]][guards[i][1]] == 0) ans--;
+            v[guards[i][0]][guards[i][1]] = 2;
+
+            int row = guards[i][0], col = guards[i][1];
             for(int i=0; i<4; i++){
                 int nr = row + dr[i];
                 int nc = col + dc[i];
@@ -31,19 +30,21 @@ public:
                 v[nr][nc] = 1;
                 q.push({nr, nc, i});
             }
-            while(q.size()){
-                node now = q.front();
-                q.pop();
+        }
+        
+        while(q.size()){
+            node now = q.front();
+            q.pop();
+            if(v[now.row][now.col] == 2) continue;
 
-                int nr = now.row + dr[now.dir];
-                int nc = now.col + dc[now.dir];
-                if(nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
-                if(v[nr][nc] > 1) continue;
+            int nr = now.row + dr[now.dir];
+            int nc = now.col + dc[now.dir];
+            if(nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
+            if(v[nr][nc] > 1) continue;
 
-                if(v[nr][nc] == 0) ans--;
-                v[nr][nc] = 1;
-                q.push({nr, nc, now.dir});
-            }
+            if(v[nr][nc] == 0) ans--;
+            v[nr][nc] = 1;
+            q.push({nr, nc, now.dir});
         }
         return ans;
     }
