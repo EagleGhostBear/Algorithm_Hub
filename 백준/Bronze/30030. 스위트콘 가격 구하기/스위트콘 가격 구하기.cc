ffecf6b22d008742b75e6e937d7ce865b2aaadaf
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-11-11 23:53:38 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    cout << n / 11 * 10;
+}
