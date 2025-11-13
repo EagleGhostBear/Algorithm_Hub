0ee9c63dd8eeabc7544157c217e9243878d8e0ed
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-11-13 13:59:33 +0900

    [Medium] Title: 3228-maximum-number-of-operations-to-move-ones-to-the-end, Time: 7 ms (43.12%), Memory: 15.8 MB (75%) -LeetHub-3.0

--- LeetCode/Medium/3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ LeetCode/Medium/3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int ans = 0;
-        pair<int, int> cnt = {0, 0}; // 지나간 0, 1의 개수
+        int ans = 0, cnt = 0; // 지나간 1의 개수
         bool chk = true;
 
         for(int i=0; i<s.size(); i++){
             if(s[i] == '0'){
-                if(chk) ans += cnt.second;
+                if(chk) ans += cnt;
                 chk = false;
             }
-            else{
-                cnt.second++;
-                chk = true;
-            }
+            else{ cnt++; chk = true; }
         }
         return ans;
     }
