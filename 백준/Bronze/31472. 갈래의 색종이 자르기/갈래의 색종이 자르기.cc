74c3400c14f36074b6dfc37d0f2a85818583fada
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-11-15 23:14:05 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    cout << sqrt(n * 2) * 4;
+}
