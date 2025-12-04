7c7a204ed02e07b2d215f63e987fe733530aa0fa
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-12-05 02:04:44 +0900

    [Easy] Title: 0035-search-insert-position, Time: 0 ms (100%), Memory: 13.6 MB (76.61%) -LeetHub-3.0

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.cpp
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,18 +4,15 @@ public:
         int left = 0;
         int right = nums.size() - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target){
+                left = mid;
+                break;
             }
+            else if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
         }
-
-        return left;        
+        return left;
     }
 };
