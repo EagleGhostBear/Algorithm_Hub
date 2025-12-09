6ed971681341851586571671d281d188f8654b90
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-12-09 14:47:42 +0900

    [Medium] Title: 3583-count-special-triplets, Time: 2555 ms (5.1%), Memory: 330.8 MB (87.96%) -LeetHub-3.0

--- LeetCode/Medium/3583-count-special-triplets/3583-count-special-triplets.cpp
+++ LeetCode/Medium/3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -4,30 +4,31 @@ class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
         int ans = 0, rem = 1e9 + 7;
-        unordered_map<int, vector<int>> um;
-        for(int i=0; i<nums.size(); i++) um[nums[i]].push_back(i);
-        for(auto c : um){
-            int jVal = c.first;
-            int ikVal = c.first * 2;
-            if(um.find(ikVal) == um.end()) continue;
-            int sz = um[ikVal].size();
-            if(c.first == 0){
-                for(ll i=0; i<sz; i++){
-                    ll tmp = i * (sz - i - 1) % rem;
-                    ans = (ans + tmp) % rem;
-                }
-                continue;
+        vector<vector<int>> v(1e5 + 1, vector<int>());
+        for(int i=0; i<nums.size(); i++) v[nums[i]].push_back(i);
+        if(v[0].size()){
+            int sz = v[0].size();
+            for(ll i=0; i<sz; i++){
+                ll tmp = i * (sz - i - 1) % rem;
+                ans = (ans + tmp) % rem;
             }
-            vector<int> cnt(c.second.size(), 0); // j 기준으로 더 작은게 몇개인가
+        }
+        for(int i=1; i<v.size(); i++){
+            int jVal = i;
+            int ikVal = i * 2;
+            if(ikVal > 1e5 || v[ikVal].size() < 2) continue;
+            int sz = v[ikVal].size();
+
+            vector<int> cnt(v[jVal].size(), 0); // j 기준으로 더 작은게 몇개인가
             int idx = 0;
-            for(int i=0; i<c.second.size(); i++){
-                if(i) cnt[i] = cnt[i-1];
-                int now = c.second[i];
+            for(int j=0; j<v[jVal].size(); j++){
+                if(j) cnt[j] = cnt[j-1];
+                int now = v[jVal][j];
                 for(; idx<sz; idx++){
-                    if(um[ikVal][idx] < now) cnt[i]++;
+                    if(v[ikVal][idx] < now) cnt[j]++;
                     else break;
                 }
-                ans = (ans + cnt[i] * (sz - cnt[i])) % rem;
+                ans = (ans + cnt[j] * (sz - cnt[j])) % rem;
             }
         }
         return ans;
