13dbfb3922ade3dcf26aaf6c2000a8de4641632f
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-12-09 16:09:55 +0900

    [Medium] Title: 3583-count-special-triplets, Time: 354 ms (92.7%), Memory: 310.5 MB (91.24%) -LeetHub-3.0

--- LeetCode/Medium/3583-count-special-triplets/3583-count-special-triplets.cpp
+++ LeetCode/Medium/3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -3,34 +3,15 @@ typedef long long ll;
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        int ans = 0, rem = 1e9 + 7;
-        vector<vector<int>> v(1e5 + 1, vector<int>());
-        for(int i=0; i<nums.size(); i++) v[nums[i]].push_back(i);
-        if(v[0].size()){
-            int sz = v[0].size();
-            for(ll i=0; i<sz; i++){
-                ll tmp = i * (sz - i - 1) % rem;
-                ans = (ans + tmp) % rem;
-            }
+        ll ans = 0, rem = 1e9 + 7;
+        vector<int> prev(1e5 + 1, 0);
+        vector<int> total(1e5 + 1, 0);
+        for(int i=0; i<nums.size(); i++) total[nums[i]]++;
+        for(int i=0; i<nums.size(); i++){
+            int val = nums[i], val2 = (val << 1);
+            if(val2 <= 1e5) ans += (ll)prev[val2] * (total[val2] - prev[val2] -(val == 0));
+            prev[val]++;
         }
-        for(int i=1; i<v.size(); i++){
-            int jVal = i;
-            int ikVal = i * 2;
-            if(ikVal > 1e5 || v[ikVal].size() < 2) continue;
-            int sz = v[ikVal].size();
-
-            vector<int> cnt(v[jVal].size(), 0); // j 기준으로 더 작은게 몇개인가
-            int idx = 0;
-            for(int j=0; j<v[jVal].size(); j++){
-                if(j) cnt[j] = cnt[j-1];
-                int now = v[jVal][j];
-                for(; idx<sz; idx++){
-                    if(v[ikVal][idx] < now) cnt[j]++;
-                    else break;
-                }
-                ans = (ans + cnt[j] * (sz - cnt[j])) % rem;
-            }
-        }
-        return ans;
+        return ans % rem;
     }
 };
