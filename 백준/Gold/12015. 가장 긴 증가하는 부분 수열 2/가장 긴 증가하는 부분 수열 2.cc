76f8fe7d2b6e10608bb52460cf850a9ab02b0610
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-12-24 20:54:15 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,57 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <string>
-#include <cmath>
 
 using namespace std;
 
 typedef long long ll;
 
-int n;
-vector<int> v;
-
-int solution(int target) { // 이분탐색으로 target과 같거나 가장 근접하게 큰 수를 찾는다.
-    int left = 0;
-    int right = v.size() - 1;
-    int mid;
-
-    while (left <= right) {
-        mid = (left + right) / 2;
-        if (left == right) {
-            break;
-        }
-        else if (target <= v[mid]) { // 큰것 중에 가장 근접한 수
-            right = mid;
-        }
-        else {
-            left = mid + 1;
-        }
-    }
-    return mid;
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
+    int n;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
+    vector<int> lis;
 
-        if (!v.empty() && tmp <= v.back()) {
-            int idx = solution(tmp);
-            //cout << "change: " << v[idx] << " -> " << tmp << "\n";
-            v[idx] = tmp;
-        }
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if (lis.empty() || lis.back() < num) lis.push_back(num);
         else {
-            v.push_back(tmp);
+            int left = 0, right = lis.size() - 1;
+            int mid = 0;
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (lis[mid] == num) break;
+                else if (lis[mid] < num) left = mid + 1;
+                else right = mid - 1;
+            }
+            if (lis[mid] < num) lis[mid + 1] = num;
+            else lis[mid] = num;
         }
     }
-
-    cout << v.size();
+    cout << lis.size();
 }
