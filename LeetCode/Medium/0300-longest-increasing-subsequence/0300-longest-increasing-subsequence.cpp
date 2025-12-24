115397bcab27031a3b7bc07e9f401b9c51803749
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-12-24 19:08:04 +0900

    [Medium] Title: 0300-longest-increasing-subsequence, Time: 59 ms (62.55%), Memory: 14.5 MB (38.33%) -LeetHub-3.0

---
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        dp[0] = 1;
+        for(int i=1; i<nums.size(); i++){
+            for(int j=i-1; j>-1; j--){
+                if(nums[i] > nums[j]) dp[i] = max(dp[i], dp[j] + 1);
+            } 
+        }
+        int ans = 1;
+        for(auto n : dp) ans = max(ans, n);
+        return ans;
+    }
+};
