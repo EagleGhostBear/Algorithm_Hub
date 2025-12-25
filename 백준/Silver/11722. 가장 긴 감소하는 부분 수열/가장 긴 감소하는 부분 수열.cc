da30e2e19557072ca477cc532215556547063391
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2025-12-25 19:00:42 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> lis;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if (lis.empty() || lis.back() > num) lis.push_back(num);
+        else {
+            int left = 0, right = lis.size() - 1;
+            int mid = 0;
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (left == right) break;
+                else if (num >= lis[mid]) right = mid;
+                else left = mid + 1;
+            }
+            lis[mid] = num;
+        }
+    }
+    cout << lis.size();
+}
