8820a717b0a381e50427afe6895ee891ee9d67ec
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-01-01 16:10:20 +0900

    [Easy] Title: 0066-plus-one, Time: 0 ms (100%), Memory: 11.3 MB (96.43%) -LeetHub-3.0

--- LeetCode/Easy/0066-plus-one/0066-plus-one.cpp
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int val = 1;
+        vector<int> ans;
+        int up = 1;
         for(int i=digits.size() - 1; i>-1; i--){
-            digits[i] += val;
-            val = 0;
-            if(digits[i] == 10){
+            digits[i] += up;
+            up = 0;
+            if(digits[i] > 9){
                 digits[i] = 0;
-                val = 1;
-            } 
+                up = 1;
+            }
         }
-        if(val) digits.insert(digits.begin(), 1);
-        return digits;
+        if(up) ans.push_back(1);
+        for(auto n : digits) ans.push_back(n);
+        return ans;
     }
 };
