96ad644d20c31a620c02126393b16e8dbf3b42eb
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-01-06 10:40:19 +0900

    [Medium] Title: 1161-maximum-level-sum-of-a-binary-tree, Time: 6 ms (35.3%), Memory: 112 MB (10.98%) -LeetHub-3.0

--- LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -30,7 +30,6 @@ public:
             if(now.node->right) q.push({now.level + 1, now.node->right});
         }
         pair<int, int> ans = {-1e5, -1};
-        for(int i=0; i<sumLevel.size(); i++) cout << sumLevel[i] << "\n";
         for(int i=0; i<sumLevel.size(); i++) if(ans.first < sumLevel[i]) ans = {sumLevel[i], i + 1};
         return ans.second;
     }
