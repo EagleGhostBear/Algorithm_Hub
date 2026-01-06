27d048671e3d6a1b6e9a7b98398cfadf1e34bd75
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-01-06 10:37:59 +0900

    [Medium] Title: 1161-maximum-level-sum-of-a-binary-tree, Time: 18 ms (5.68%), Memory: 112.2 MB (6.93%) -LeetHub-3.0

--- LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -17,17 +17,20 @@ public:
     };
 
     int maxLevelSum(TreeNode* root) {
-        vector<int> sumLevel(101, 0);
+        vector<int> sumLevel;
         queue<tree> q;
         q.push({0, root});
         while(q.size()){
             tree now = q.front(); q.pop();
 
+            // 최대 100개 까지만 만들기 때문에 그때그때 만드는게 더 효율적일것
+            while(sumLevel.size() < now.level + 1) sumLevel.push_back(0);
             sumLevel[now.level] += now.node->val;
             if(now.node->left) q.push({now.level + 1, now.node->left});
             if(now.node->right) q.push({now.level + 1, now.node->right});
         }
         pair<int, int> ans = {-1e5, -1};
+        for(int i=0; i<sumLevel.size(); i++) cout << sumLevel[i] << "\n";
         for(int i=0; i<sumLevel.size(); i++) if(ans.first < sumLevel[i]) ans = {sumLevel[i], i + 1};
         return ans.second;
     }
