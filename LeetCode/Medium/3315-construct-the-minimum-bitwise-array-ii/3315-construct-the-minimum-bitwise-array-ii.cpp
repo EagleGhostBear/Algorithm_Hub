6bd138fc50240aad17eb38b7d420bd18979d925a
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-01-21 22:02:36 +0900

    [Medium] Title: 3315-construct-the-minimum-bitwise-array-ii, Time: 7 ms (6.8%), Memory: 28.3 MB (31.07%) -LeetHub-3.0

--- LeetCode/Medium/3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ LeetCode/Medium/3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -7,12 +7,16 @@ public:
             int val = 1;
             while((val & nums[i]) == val) val = val * 2 + 1;
             val /= 2;
-            if(nums[i] - val){
-                if(val > 1) ans.push_back(nums[i] - val + (val / 2));
-                else ans.push_back(nums[i] - 1);
-            }
-            else ans.push_back(val / 2);
+            // ex) 101111 -> 100111 : 오른쪽 연속된 1을 1자리 줄인게 정답
+            // 10000(nums[i] - val) + 111(val / 2)
+            if(nums[i] - val) ans.push_back(nums[i] - val + (val / 2));
+            else ans.push_back(val / 2); // 2^n - 1 의 형태
         }
         return ans;
     }
 };
+/*
+1. 구조적으로 짝수는 불가
+2. 자리수가 바뀔려면 전부 1 인 경우 밖에 없음 -> 1111 || 10000 -> 2^n - 1 의 형태
+3. 오른쪽에서 1이 얼마나 이어지는가?
+*/
