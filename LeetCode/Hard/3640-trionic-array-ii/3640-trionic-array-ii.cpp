5993048e92d090a45614a2c43d913aae197c4e7e
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-02-04 15:01:17 +0900

    [Hard] Title: 3640-trionic-array-ii, Time: 3 ms (93.64%), Memory: 132.6 MB (91.82%) -LeetHub-3.0

--- LeetCode/Hard/3640-trionic-array-ii/3640-trionic-array-ii.cpp
+++ LeetCode/Hard/3640-trionic-array-ii/3640-trionic-array-ii.cpp
@@ -1,46 +1,37 @@
-typedef long long ll;
-
 class Solution {
 public:
     long long maxSumTrionic(vector<int>& nums) {
-        ll ans = -1e15;
-        vector<ll> weight(4, -1e15);
-        for(int i=0; i<nums.size(); i++){
+        long long ans = -1e15;
+        long long w1, w2, w3;
+        w1 = w2 = w3 = -1e15;
+        for(int i=1; i<nums.size(); i++){
             int diff = (i ? nums[i] - nums[i - 1] : 0);
-            // cout << "now: " << nums[i] << " diff: " << diff << "\n";
-            // for(auto c : weight) cout << c << " "; cout << "\n";
-            if(diff == 0){
-                ans = max(ans, weight[3]);
-                weight.assign(4, -1e15);
-            }
+            if(diff == 0) w1 = w2 = w3 = -1e15;
             else if(diff > 0){
                 // 3
-                weight[3] += nums[i];
+                w3 += nums[i];
                 // 2
-                weight[3] = max(weight[3], weight[2] + nums[i]);
-                weight[2] = -1e15;
+                w3 = max(w3, w2 + nums[i]);
+                w2 = -1e15;
                 // 1
-                weight[1] += nums[i];
+                w1 += nums[i];
                 // 0
-                weight[1] = max(weight[1], weight[0] + nums[i]);
-                // weight[0] = -1e15;
+                w1 = max(w1, (long long)nums[i - 1] + nums[i]);
             }
             else{
                 // 3
-                ans = max(ans, weight[3]); weight[3] = -1e15;
+                w3 = -1e15;
                 // 2
-                weight[2] += nums[i];
+                w2 += nums[i];
                 // 1
-                weight[2] = max(weight[2], weight[1] + nums[i]);
-                weight[1] = -1e15;
+                w2 = max(w2, w1 + nums[i]);
+                w1 = -1e15;
             }
-            weight[0] = nums[i];
-            ans = max(ans, weight[3]);
+            ans = max(ans, w3);
         }
         return ans;
     }
 };
 /*
-증, 감, 증, 감, 증 -> 이렇듯 3번째 증은 양측에 사용될 수 있다.
-따로 배열을 만들어서 값들을 넣고 조건에 안맞는건 빼고 맞는건 계속 갱신해 나간다?
+증, 증감, 증감증 -> 각 상태는 중복해서 존재할 필요가 없다. 해당 상태를 단일로 관리한다.
 */
