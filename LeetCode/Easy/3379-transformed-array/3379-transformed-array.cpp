21b3bf15d3b794df286fd5b5c54bf02f39acb45d
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-02-05 10:34:17 +0900

    [Easy] Title: 3379-transformed-array, Time: 8 ms (46.52%), Memory: 25.6 MB (70.75%) -LeetHub-3.0

--- LeetCode/Easy/3379-transformed-array/3379-transformed-array.cpp
+++ LeetCode/Easy/3379-transformed-array/3379-transformed-array.cpp
@@ -4,10 +4,7 @@ public:
         vector<int> ans;
         int sz = nums.size();
         for(int i=0; i<sz; i++){
-            int val = abs(nums[i]) % sz;
-            int idx = (nums[i] >= 0 ? i + val : i - val);
-            if(idx >= sz) idx -= sz;
-            if(idx < 0) idx += sz;
+            int idx = ((i + nums[i]) % sz + sz) % sz;
             ans.push_back(nums[idx]);
         }
         return ans;
