d3599194c02f464be1ff948902671551342456b5
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-02-06 10:31:36 +0900

    [Medium] Title: 3634-minimum-removals-to-balance-array, Time: 39 ms (32.85%), Memory: 105.1 MB (14.24%) -LeetHub-3.0

--- LeetCode/Medium/3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ LeetCode/Medium/3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -1,26 +1,12 @@
-typedef long long ll;
-
 class Solution {
 public:
-    int binary_search(int startIdx, ll target, vector<int>& nums){
-        int left = startIdx, right = nums.size() - 1;
-        int mid;
-        while(left <= right){
-            mid = (left + right) / 2;
-            if(nums[mid] <= target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return mid;
-    }
     int minRemoval(vector<int>& nums, int k) {
-        int ans = 1e9;
-        vector<int> v(nums.size());
+        int ans = 1e9, sz = nums.size();
         sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size(); i++){
-            int idx = binary_search(i, (ll)nums[i] * k, nums);
-            if((ll)nums[i] * k < nums[idx]) idx--;
-            int cnt = idx - i + 1;
-            ans = min(ans, (int)nums.size() - cnt);
+        int right = 0;
+        for(int i=0; i<sz; i++){
+            for(;right<sz; right++) if((long long)nums[i] * k < nums[right]) break;
+            ans = min(ans, sz - right + i);
         }
         return ans;
     }
