fa0c79f4d1c88b7ef83abc00435e9193c5251027
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-02-27 08:53:20 +0900

    [Medium] Title: 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one, Time: 1 ms (22.39%), Memory: 8.8 MB (83.83%) -LeetHub-3.0

--- LeetCode/Medium/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ LeetCode/Medium/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int numSteps(string s) {
-        int num = 0, sz = s.size();
-        for(int i=0; i<sz; i++){
-            if(s[sz - 1 - i] == '1') num += pow(2, i);
-        }
-        int ans = 0;
-        while(num != 1){
-            if(num % 2) {num += 1; ans++;}
-            num /= 2;
+        int ans = 0, up = 0;
+        for(int i=s.size() - 1; i>-1; i--){
+            if(i == 0 && up == 0) break;
+            int now = s[i] - '0' + up; up = 0;
+            if(now == 2) up++, now = 0;
+            else if(now == 1) ans++, up++;
             ans++;
         }
         return ans;
