6676548c1648ccff5094b89ca5d550750af28b59
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-03-05 10:04:17 +0900

    [Easy] Title: 1582-special-positions-in-a-binary-matrix, Time: 3 ms (19.39%), Memory: 18.7 MB (20.82%) -LeetHub-3.0

--- LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
-    static vector<int> check1(vector<int>& vect, int sz){
-        vector<int> ans;
-        for(int i=0; i<sz; i++){
-            if (vect[i]==1) ans.push_back(i);
+    int numSpecial(vector<vector<int>>& mat) {
+        int ans = 0;
+        vector<vector<pair<int, int>>> row(mat.size(), vector<pair<int, int>>()), col(mat[0].size(), vector<pair<int, int>>());
+        for(int i=0; i<mat.size(); i++) for(int j=0; j<mat[i].size(); j++){
+            if(mat[i][j]){
+                row[i].push_back({i, j});
+                col[j].push_back({i, j});
+            }
         }
-        return ans;
-    }
-    static int numSpecial(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size(), ans=0;
-        vector<bool> col(n, 0);//already check or not
-        for(int i=0; i<m; i++){
-            auto idx=check1(mat[i], n);
-            int j;
-            if (idx.size()==1 && col[(j=idx[0])]==0){
-                col[j]=1;
-                int count_col1=0;
-                for(int k=0; k<m; k++)
-                    count_col1+=mat[k][j];
-                if (count_col1==1){
-                //    cout<<"("<<i<<","<<j<<")\n";
-                    ans++;
-                }     
-            }
-            else{
-                for(int j: idx)
-                    col[j]=1;
-            }
+        for(int i=0; i<row.size(); i++){
+            if(row[i].size() != 1) continue;
+            if(col[row[i][0].second].size() != 1) continue;
+            ans++;
         }
         return ans;
     }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
