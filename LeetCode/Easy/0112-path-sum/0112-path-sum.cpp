587b31528ab06c8261566f9336896f8ff919a006
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-03-12 22:40:15 +0900

    [Easy] Title: 0112-path-sum, Time: 1 ms (5.63%), Memory: 21.4 MB (94.5%) -LeetHub-3.0

---
+++ LeetCode/Easy/0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans = false;
+    void dfs(TreeNode* tree, int tot, int& targetSum){
+        if(tree->left == nullptr && tree->right == nullptr){
+            if(targetSum == tot) ans = true;
+        }
+        else{
+            if(tree->left) dfs(tree->left, tot + tree->left->val, targetSum);
+            if(tree->right) dfs(tree->right, tot + tree->right->val, targetSum);
+        }
+        return;
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root) dfs(root, root->val, targetSum);
+        return ans;
+    }
+};
