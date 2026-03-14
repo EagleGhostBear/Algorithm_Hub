c2dc76935dc8bf54fa456e85dc0728109cb5772f
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-03-14 17:01:37 +0900

    [Medium] Title: 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n, Time: N/A (0%), Memory: N/A (0%) -LeetHub-3.0

--- LeetCode/Medium/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ LeetCode/Medium/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        string s = "";
-        vector<int> ans;
-        int cnt = 3 * pow(2, n - 1); //1536
-        if(k > cnt) return s;
+        string ans = "";
+        int cnt = 3 * (1 << (n - 1)); //1536
+        if(k > cnt) return ans;
         vector<int> v(n);
         v.back() = 1;
         for(int i=v.size() - 2; i>-1; i--) v[i] = v[i + 1] * 2;
-        for(int i=0; i<n; i++){
-            if(i == 0){
-                for(int j=0; j<3; j++) if(k <= v[i] * (j + 1)){
-                    k -= v[i] * j;
-                    ans.push_back(j);
-                    break;
-                }
-            }
-            else{
-                if(ans.back() == 0){
-                    for(int j=0; j<2; j++) if(k <= v[i] * (j + 1)){
-                        k -= v[i] * j;
-                        ans.push_back(j + 1);
-                        break;
-                    }
-                }
-                else if(ans.back() == 1){
-                    for(int j=0; j<2; j++) if(k <= v[i] * (j + 1)){
-                        k -= v[i] * j;
-                        if(j == 0) ans.push_back(j);
-                        else ans.push_back(j + 1);
-                        break;
-                    }
-                }
-                else if(ans.back() == 2){
-                    for(int j=0; j<2; j++) if(k <= v[i] * (j + 1)){
-                        k -= v[i] * j;
-                        ans.push_back(j);
-                        break;
-                    }
-                }
+        // 첫번째자리 3가지 가능성
+        for(int j=0; j<3; j++) if(k <= v[0] * (j + 1)){
+            k -= v[0] * j;
+            ans += 'a' + j;
+            break;
+        }
+        // 그이외 2가지 가능성
+        for(int i=1; i<n; i++){
+            for(int j=0; j<2; j++) if(k <= v[i] * (j + 1)){
+                k -= v[i] * j;
+                if(ans.back() == 'a' || (ans.back() == 'b' && j == 1)) ans += 'a' + j + 1;
+                else ans += 'a' + j;
+                break;
             }
         }
-        for(auto c : ans) s += ('a' + c);
-        return s;
+        return ans;
     }
 };
