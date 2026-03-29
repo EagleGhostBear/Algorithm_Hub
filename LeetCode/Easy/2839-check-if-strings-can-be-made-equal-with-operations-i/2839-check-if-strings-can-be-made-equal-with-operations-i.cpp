f5b9cc310adcae4915f2359c0e461fe8428a27f3
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-03-29 23:33:42 +0900

    [Easy] Title: 2839-check-if-strings-can-be-made-equal-with-operations-i, Time: 3 ms (14.84%), Memory: 9.6 MB (5.81%) -LeetHub-3.0

---
+++ LeetCode/Easy/2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        vector<vector<int>> alp1(2, vector<int>(26, 0));
+        vector<vector<int>> alp2(2, vector<int>(26, 0));
+        for(int i=0; i<s1.size(); i++){
+            int val = i % 2;
+            alp1[val][s1[i] - 'a']++;
+            alp2[val][s2[i] - 'a']++;
+        }
+        for(int i=0; i<26; i++) if(alp1[0][i] != alp2[0][i] || alp1[1][i] != alp2[1][i]) return false;
+        return true;
+    }
+};
