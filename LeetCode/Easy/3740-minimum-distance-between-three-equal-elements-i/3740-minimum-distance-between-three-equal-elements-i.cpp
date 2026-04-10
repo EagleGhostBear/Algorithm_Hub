cd506dbd200a1a70f8a1bf7f9a9c0c85775fea98
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-04-10 23:40:52 +0900

    [Easy] Title: 3740-minimum-distance-between-three-equal-elements-i, Time: 4 ms (52.52%), Memory: 31.7 MB (7.35%) -LeetHub-3.0

---
+++ LeetCode/Easy/3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int ans = 1e9;
+        vector<vector<int>> v(101, vector<int>());
+        for(int i=0; i<nums.size(); i++){
+            v[nums[i]].push_back(i);
+        }
+        for(int i=0; i<=100; i++){
+            if(v[i].size() < 3) continue;
+            for(int j=0; j<v[i].size()-2; j++){
+                ans = min(ans, abs(v[i][j] - v[i][j + 1]) + abs(v[i][j] - v[i][j + 2]) + abs(v[i][j + 1] - v[i][j + 2]));
+            }
+        }
+        if(ans >= 1e9) return -1;
+        return ans;
+    }
+};
