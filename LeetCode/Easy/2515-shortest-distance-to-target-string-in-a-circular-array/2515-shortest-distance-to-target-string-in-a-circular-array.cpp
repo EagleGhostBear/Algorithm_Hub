596da120b1edc61c25238fbbde1c1c4717f6d4f0
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-04-15 18:58:14 +0900

    [Easy] Title: 2515-shortest-distance-to-target-string-in-a-circular-array, Time: N/A (0%), Memory: N/A (0%) -LeetHub-3.0

--- LeetCode/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ LeetCode/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -3,9 +3,9 @@ public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
         int sz = words.size() / 2;
         for(int i=0; i<=sz; i++){
-            string left = (startIndex - i < 0 ? words[startIndex - i + words.size()] : words[startIndex - i]);
-            string right = (startIndex + i >= words.size() ? words[startIndex + i - words.size()] : words[startIndex + i]);
-            if(left == target || right == target) return i;
+            int left = (startIndex - i < 0 ? startIndex - i + words.size() : startIndex - i);
+            int right = (startIndex + i >= words.size() ? startIndex + i - words.size() : startIndex + i);
+            if(words[left] == target || words[right] == target) return i;
         }
         return -1;
     }
