404df9dcaa4b160cdb250573ea2193bd10048c7a
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-04-24 23:06:09 +0900

    [Easy] Title: 2833-furthest-point-from-origin, Time: 0 ms (100%), Memory: 9 MB (65.27%) -LeetHub-3.0

---
+++ LeetCode/Easy/2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int ans = 0, cnt = 0;
+        for(int i=0; i<moves.size(); i++){
+            if(moves[i] == 'L') ans--;
+            else if(moves[i] == 'R') ans++;
+            else cnt++;
+        }
+        return abs(ans) + cnt;
+    }
+};
