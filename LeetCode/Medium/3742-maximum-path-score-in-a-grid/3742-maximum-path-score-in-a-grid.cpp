25f7397e15ee66d7a52562ebc61aa01fd4711f7c
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-04-30 11:45:24 +0900

    [Medium] Title: 3742-maximum-path-score-in-a-grid, Time: N/A (0%), Memory: N/A (0%) -LeetHub-3.0

--- LeetCode/Medium/3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ LeetCode/Medium/3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -3,25 +3,32 @@ public:
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int ans = -1e4;
         int rs = grid.size(), cs = grid[0].size();
-        if(rs == 1 && cs == 1) return 0;
-        vector<vector<vector<int>>> dp(rs, vector<vector<int>>(cs, vector<int>(k + 1, -1e4)));
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(cs, vector<int>(k + 1, -1e4))); // 2 * 200 * 1000 = 40,000,000
         dp[0][0][k] = 0;
-        for(int i=0; i<rs; i++) for(int j=0; j<cs; j++){
-            if(grid[i][j]){
-                // 상
-                if(i) for(int t=k - 1; t>-1; t--) dp[i][j][t] = max(dp[i][j][t], dp[i - 1][j][t + 1] + grid[i][j]);
-                // 좌
-                if(j) for(int t=k - 1; t>-1; t--) dp[i][j][t] = max(dp[i][j][t], dp[i][j - 1][t + 1] + grid[i][j]);
-            }
-            else{
-                // 상
-                if(i) for(int t=k; t>-1; t--) dp[i][j][t] = max(dp[i][j][t], dp[i - 1][j][t]);
-                if(j) for(int t=k; t>-1; t--) dp[i][j][t] = max(dp[i][j][t], dp[i][j - 1][t]);
-            }
+        for(int i=1; i<cs; i++){ // row = 0 정의
+            if(grid[0][i]) for(int t=k - 1; t>-1; t--) dp[0][i][t] = max(dp[0][i][t], dp[0][i - 1][t + 1] + grid[0][i]);
+            else for(int t=k; t>-1; t--) dp[0][i][t] = max(dp[0][i][t], dp[0][i - 1][t]);
         }
-        for(int i=0; i<=k; i++) ans = max(ans, dp[rs - 1][cs - 1][i]);
+        for(int i=1; i<rs; i++){
+            for(int j=0; j<cs; j++){
+                if(grid[i][j]){
+                    // 상
+                    for(int t=k - 1; t>-1; t--) dp[1][j][t] = max(dp[1][j][t], dp[0][j][t + 1] + grid[i][j]);
+                    // 좌
+                    if(j) for(int t=k - 1; t>-1; t--) dp[1][j][t] = max(dp[1][j][t], dp[1][j - 1][t + 1] + grid[i][j]);
+                }
+                else{
+                    // 상
+                    for(int t=k; t>-1; t--) dp[1][j][t] = max(dp[1][j][t], dp[0][j][t]);
+                    // 좌
+                    if(j) for(int t=k; t>-1; t--) dp[1][j][t] = max(dp[1][j][t], dp[1][j - 1][t]);
+                }
+            }
+            dp[0] = dp[1];
+            dp[1].assign(cs, vector<int>(k + 1, -1e4));
+        }
+        for(int i=0; i<=k; i++) ans = max(ans, dp[0][cs - 1][i]);
         if(ans < 0) return -1;
         return ans;
     }
 };
-// 200 * 200 * 1000
