20495cc304dc0da437b986d10128c131ba777a28
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-05-14 20:05:13 +0900

    [Easy] Title: 2784-check-if-array-is-good, Time: 0 ms (100%), Memory: 27.3 MB (31.44%) -LeetHub-3.0

--- LeetCode/Easy/2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ LeetCode/Easy/2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -6,7 +6,6 @@ public:
             if(n >= nums.size()) return false;
             v[n]++;
         }
-        for(auto n : v) cout << n << " ";
         for(int i=1; i<v.size() - 1; i++) if(v[i] != 1) return false;
         if(v.back() != 2) return false;
         return true;
