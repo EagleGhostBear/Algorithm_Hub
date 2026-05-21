b1db6cd3d104d056d696a65363bc199d06280a99
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-05-21 11:09:24 +0900

    [Medium] Title: 3043-find-the-length-of-the-longest-common-prefix, Time: 194 ms (42.64%), Memory: 153.5 MB (41.24%) -LeetHub-3.0

--- LeetCode/Medium/3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ LeetCode/Medium/3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
+    int getLen(int n) {
+        if (n == 0) return 1; // log10(0)은 예외 처리가 필요함
+        return log10(n) + 1;
+    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         int ans = 0;
-        unordered_map<string, bool> um;
+        unordered_map<int, bool> um;
         for(int i=0; i<arr1.size(); i++){ // 400,000
-            string s = to_string(arr1[i]);
-            for(int len=1; len<=s.size(); len++){
-                string now = s.substr(0, len);
+            int now = arr1[i];
+            while(now){
                 um[now] = true;
+                now /= 10;
             }
         }
         for(int i=0; i<arr2.size(); i++){
-            string s = to_string(arr2[i]);
-            for(int len=s.size(); len>ans; len--){
-                string now = s.substr(0, len);
+            int now = arr2[i], len = getLen(now);
+            while(now && len > ans){
                 if(um[now]) {ans = len; break;}
+                now /= 10;
+                len--;
             }
         }
         return ans;
