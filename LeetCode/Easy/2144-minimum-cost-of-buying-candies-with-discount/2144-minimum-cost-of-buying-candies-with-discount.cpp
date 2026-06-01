c1d680b548c40d60cd39f20e153c93390aaaad3e
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-01 11:13:56 +0900

    [Easy] Title: 2144-minimum-cost-of-buying-candies-with-discount, Time: 0 ms (100%), Memory: 14.3 MB (52.76%) -LeetHub-3.0

--- LeetCode/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ LeetCode/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -2,17 +2,8 @@ class Solution {
 public:
     int minimumCost(vector<int>& cost) {
         int ans = 0;
-        priority_queue<int> pq;
-        for(auto n : cost) pq.push(n);
-        while(pq.size()){
-            if(pq.size() >= 3){
-                for(int i=0; i<2; i++) {ans += pq.top(); pq.pop();}
-                pq.pop();
-            }
-            else{
-                while(pq.size()) {ans += pq.top(); pq.pop();}
-            }
-        }
+        sort(cost.begin(), cost.end(), greater<int>());
+        for(int i=0; i<cost.size(); i++) if(i % 3 != 2) ans += cost[i];
         return ans;
     }
 };
