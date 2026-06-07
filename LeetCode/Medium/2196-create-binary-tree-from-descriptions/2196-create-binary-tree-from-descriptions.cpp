3313c762eb86295d3bc86abbd20fbed6d95c9106
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-07 19:51:30 +0900

    [Medium] Title: 2196-create-binary-tree-from-descriptions, Time: 424 ms (5.27%), Memory: 308.2 MB (8.41%) -LeetHub-3.0

---
+++ LeetCode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> um;
+        unordered_map<int, int> parent; // {child, parent}
+        unordered_map<int, bool> pt;
+        for(int i=0; i<descriptions.size(); i++){
+            if(pt[descriptions[i][0]] && pt[descriptions[i][1]]) int t = 1;
+            pt[descriptions[i][1]] = true;
+            parent[descriptions[i][1]] = descriptions[i][0];
+            TreeNode* now = new TreeNode(descriptions[i][0]);
+
+            // 부모가 이미 정의된게 있는지 확인
+            if(um[descriptions[i][0]] != nullptr) now = um[descriptions[i][0]];
+
+            // 자식이 이미 정의된게 있는지 확인
+            if(descriptions[i][2]){
+                if(um[descriptions[i][1]] != nullptr) now->left = um[descriptions[i][1]];
+                else now->left = new TreeNode(descriptions[i][1]);
+            }
+            else{
+                if(um[descriptions[i][1]] != nullptr) now->right = um[descriptions[i][1]];
+                else now->right = new TreeNode(descriptions[i][1]);
+            }
+            um[descriptions[i][0]] = now;
+            while(parent[now->val]){
+                TreeNode* p = um[parent[now->val]];
+                if(p->left && p->left->val == now->val) p->left = now;
+                else if(p->right && p->right->val == now->val) p->right = now;
+                um[parent[now->val]] = p;
+                now = p;
+            }
+        }
+        TreeNode* root;
+        for(auto c : pt) if(!c.second) root = um[c.first];
+        return root;
+    }
+};
