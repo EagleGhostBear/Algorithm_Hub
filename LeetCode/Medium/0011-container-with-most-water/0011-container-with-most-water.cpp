efc2e4c2477f72412da841b4e01f56f5d55b7328
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-10 16:45:10 +0900

    [Medium] Title: 0011-container-with-most-water, Time: 0 ms (100%), Memory: 62.9 MB (79.22%) -LeetHub-3.0

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int ans = 0;
-        vector<pair<int, int>> h;
-        for(int i=0; i<height.size(); i++){ // 1e5
-            for(int j=0; j<h.size(); j++){
-                ans = max(ans, (i - h[j].first) * min(height[i], h[j].second));
-            }
-            if(h.empty() || h.back().second < height[i]) h.push_back({i, height[i]});
+        int ans = 0, left = 0, right = height.size() - 1;
+        while(left < right){
+            ans = max(ans, (right - left) * min(height[left], height[right]));
+            if(height[left] <= height[right]) left++;
+            else right--;
         }
         return ans;
     }
 };
+/*
+투포인터
+*/
