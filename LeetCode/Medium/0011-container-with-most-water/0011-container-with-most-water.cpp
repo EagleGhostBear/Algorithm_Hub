8680ade18fc795a65fef15d3061094c51739d981
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-10 16:38:49 +0900

    [Medium] Title: 0011-container-with-most-water, Time: 314 ms (6.66%), Memory: 63.2 MB (11.2%) -LeetHub-3.0

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int ans = 0;
-        vector<int> minH(1e4 + 1, 1e6);
-        vector<int> maxH(1e4 + 1, -1e6);
+        vector<pair<int, int>> h;
         for(int i=0; i<height.size(); i++){ // 1e5
-            minH[height[i]] = min(minH[height[i]], i);
-            maxH[height[i]] = max(maxH[height[i]], i);
-        }
-        for(int l=1e4; l>0; l--){ // 1e4 * 1e4
-            if(maxH[l] == -1e6) continue;
-            for(int r=l; r>0; r--){
-                if(minH[r] == 1e6) continue;
-                ans = max(ans, min(l,r) * abs(maxH[l] - minH[r]));
-                ans = max(ans, min(l,r) * abs(minH[l] - maxH[r]));
+            for(int j=0; j<h.size(); j++){
+                ans = max(ans, (i - h[j].first) * min(height[i], h[j].second));
             }
+            if(h.empty() || h.back().second < height[i]) h.push_back({i, height[i]});
         }
         return ans;
     }
