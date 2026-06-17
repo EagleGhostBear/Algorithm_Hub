6f4f7f319329e80e8a6a880bbdfbdd3ded3470d7
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-17 14:56:51 +0900

    [Medium] Title: 0015-3sum, Time: 63 ms (17.03%), Memory: 29 MB (73.55%) -LeetHub-3.0

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -6,16 +6,18 @@ public:
         
         for(int i=0; i<nums.size() - 2; i++){
             if(i && nums[i] == nums[i - 1]) continue;
-            vector<bool> visited(2e5 + 1);
             int left = i + 1, right = nums.size() - 1;
             while(left < right){
                 if(nums[i] + nums[left] + nums[right] == 0){
-                    if(!visited[nums[left] + 1e5]) ans.push_back({nums[i], nums[left], nums[right]});
-                    visited[nums[left] + 1e5] = true;
-                    left++;
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++; while(left < right && nums[left - 1] == nums[left]) left++;
                 }
-                else if(nums[i] + nums[left] + nums[right] > 0) right--;
-                else left++;
+                else if(nums[i] + nums[left] + nums[right] > 0){
+                    right--; while(left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else{
+                    left++; while(left < right && nums[left - 1] == nums[left]) left++;
+                }
             }
         }
         return ans;
