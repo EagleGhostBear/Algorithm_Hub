514ae31906ff9c3a42312b8ab091dea5078e999b
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-17 14:46:41 +0900

    [Medium] Title: 0015-3sum, Time: 137 ms (11.18%), Memory: 332.7 MB (5.34%) -LeetHub-3.0

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -3,21 +3,19 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
+        
         for(int i=0; i<nums.size() - 2; i++){
             if(i && nums[i] == nums[i - 1]) continue;
-            for(int j=i+1; j<nums.size() - 1; j++){
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int target = -1 * (nums[i] + nums[j]);
-                int left = j + 1, right = nums.size() - 1, mid;
-                while(left <= right){
-                    mid = (left + right) / 2;
-                    if(nums[mid] == target){
-                        ans.push_back({nums[i], nums[j], nums[mid]});
-                        break;
-                    }
-                    else if(nums[mid] < target) left = mid + 1;
-                    else right = mid - 1;
+            vector<bool> visited(2e5 + 1);
+            int left = i + 1, right = nums.size() - 1;
+            while(left < right){
+                if(nums[i] + nums[left] + nums[right] == 0){
+                    if(!visited[nums[left] + 1e5]) ans.push_back({nums[i], nums[left], nums[right]});
+                    visited[nums[left] + 1e5] = true;
+                    left++;
                 }
+                else if(nums[i] + nums[left] + nums[right] > 0) right--;
+                else left++;
             }
         }
         return ans;
