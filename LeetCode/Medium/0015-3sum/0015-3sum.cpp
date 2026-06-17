02fd5917f5cb24d34e9991f90d1f9687dddd77dd
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-06-17 15:03:02 +0900

    [Medium] Title: 0015-3sum, Time: 51 ms (43.24%), Memory: 29.2 MB (47.44%) -LeetHub-3.0

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int sz = nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         
-        for(int i=0; i<nums.size() - 2; i++){
+        for(int i=0; i<sz - 2; i++){
             if(i && nums[i] == nums[i - 1]) continue;
-            int left = i + 1, right = nums.size() - 1;
+            if(nums[i] > 0) break;
+            int left = i + 1, right = sz - 1;
             while(left < right){
                 if(nums[i] + nums[left] + nums[right] == 0){
                     ans.push_back({nums[i], nums[left], nums[right]});
-                    left++; while(left < right && nums[left - 1] == nums[left]) left++;
+                    left++, right--;
+                    while(left < right && nums[left - 1] == nums[left]) left++;
                 }
                 else if(nums[i] + nums[left] + nums[right] > 0){
                     right--; while(left < right && nums[right] == nums[right + 1]) right--;
