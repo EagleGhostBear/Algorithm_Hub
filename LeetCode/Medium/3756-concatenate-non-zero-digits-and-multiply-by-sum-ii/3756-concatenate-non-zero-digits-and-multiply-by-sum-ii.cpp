d397e9e4ccb1788bcdd9927b29153844f4596af7
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-07-30 14:11:15 +0900

    [Medium] Title: 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii, Time: 367 ms (5.04%), Memory: 317.2 MB (5.02%) -LeetHub-3.0

--- LeetCode/Medium/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ LeetCode/Medium/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         int mod = 1e9 + 7;
-        vector<int> ans, len(s.size()), preSum(s.size(), 0);
-        vector<long long> preMod(s.size(), 0), preGap(1e5 + 1);
-        preGap[0] = 1;
+        vector<int> ans, len(s.size() + 1, 0), preSum(s.size() + 1, 0);
+        vector<long long> preMod(s.size() + 1, 0), preGap(1e5 + 1);
+        len[0] = 0, preSum[0] = 0, preMod[0] = 0, preGap[0] = 1;
         for(int i=1; i<=1e5; i++) preGap[i] = preGap[i - 1] * 10 % mod;
         for(int i=0; i<s.size(); i++){
-            if(i){
-                len[i] = len[i - 1];
-                preSum[i] = preSum[i - 1];
-                preMod[i] = preMod[i - 1];
-            }
+                len[i + 1] = len[i];
+                preSum[i + 1] = preSum[i];
+                preMod[i + 1] = preMod[i];
             if(s[i] != '0'){
                 int num = s[i] - '0';
-                len[i]++;
-                preSum[i] += num;
-                preMod[i] = (preMod[i] * 10 + num) % mod;
+                len[i + 1]++;
+                preSum[i + 1] += num;
+                preMod[i + 1] = (preMod[i + 1] * 10 + num) % mod;
             }
         }
         for(int i=0; i<queries.size(); i++){
-            int from = queries[i][0], to = queries[i][1];
-            int totSum = (from ? preSum[to] - preSum[from - 1] : preSum[to]);
-            int gap = len[to] - (from ? len[from - 1] : 0);
-            long long tmp = (from ? preMod[from - 1] : 0) * preGap[gap] % mod;
+            int from = queries[i][0] + 1, to = queries[i][1] + 1;
+            int totSum = preSum[to] - preSum[from - 1];
+            int gap = len[to] - len[from - 1];
+            long long tmp = preMod[from - 1] * preGap[gap] % mod;
             long long val = (preMod[to] - tmp + mod) * totSum % mod;
             ans.push_back(val);
         }
