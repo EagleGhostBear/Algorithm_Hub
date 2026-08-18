0fdb116449fc40c2cae939d071207c79810f00f8
박건률 <109847507+EagleGhostBear@users.noreply.github.com>
2026-08-18 11:53:50 +0900

    [Medium] Title: 3499-maximize-active-section-with-trade-i, Time: 87 ms (61.48%), Memory: 80.9 MB (41.53%) -LeetHub-3.0

--- LeetCode/Medium/3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ LeetCode/Medium/3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int ans = 0, sz = s.size();
-        vector<pair<int, int>> v;
-        if(s[0] == '0') v.push_back({0, 1});
-        else v.push_back({1, 1});
-        for(int i=1; i<sz; i++){
-            if(s[i] == s[i - 1]) v.back().second++;
+        vector<int> zero;
+        int cnt = 0;
+        for(int i=0; i<sz; i++){
+            if(s[i] == '0') cnt++;
             else{
-                if(s[i] == '1' && s[i - 1] == '0') v.push_back({2, 1});
-                else v.push_back({s[i] - '0', 1});
-
-                if(v.back().first == 0 && v[v.size() - 2].first == 2) v[v.size() - 2].first = 3;
+                ans++;
+                if(cnt){
+                    zero.push_back(cnt);
+                    cnt = 0;
+                }
             }
         }
+        if(cnt) zero.push_back(cnt);
         int maxVal = 0;
-        for(int i=0; i<v.size(); i++){
-            if(v[i].first != 0) ans += v[i].second; // 순수 1의 개수
-            if(v[i].first == 3){ // 1반전 0반전 시킨 길이
-                maxVal = max(maxVal, v[i - 1].second + v[i + 1].second);
-            }
+        if(zero.size() > 1){
+            for(int i=0; i<zero.size() - 1; i++) maxVal = max(maxVal, zero[i] + zero[i + 1]);
         }
-        ans += maxVal;
-        return ans;
+        return ans + maxVal;
     }
 };
